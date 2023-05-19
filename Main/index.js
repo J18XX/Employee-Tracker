fabd93df8039ad9fd94b71c73f20e6071cca02a0
Guangshi Jie <guangshi18@gmail.com>
2023-05-19 12:41:57 -0700

    updated start page

---
+++ Main/index.js
@@ -0,0 +1,55 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '1234',
+});
+
+const startPage = () => {
+    inquirer.prompt({
+        message: 'What would you like to do?',
+        name: 'choice',
+        type: 'list',
+        Choices: [
+            "View All Employees",
+            "Add Employee",
+            "Update Employee Role",
+            "View All Roles",
+            "Add Role",
+            "View All Departments",
+            "Add Department",
+            "Quit",
+        ],
+    })
+    .then(response => {
+        switch(response.Choices){
+            case 'View All Employees':
+                viewAllEmployees();
+                break;
+            case 'Add Employee':
+                addEmployee();
+                break;
+            case 'Update Employee Role':
+                updateEmployeeRole();
+                break;
+            case 'View All Roles':
+                viewAllRoles();
+                break;
+            case 'Add Role':
+                addRole();
+                break;
+            case 'View All Departments':
+                viewAllDepartments();
+                break;
+            case 'Add Department':
+                addDepartment();
+                break;
+            default:
+                quit();
+        }
+    });
+};
